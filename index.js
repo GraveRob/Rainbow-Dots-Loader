22f6a5732b28f1cfbf6a19e81cca6549be44109e
GraveRob <kazan154321@gmail.com>
2021-09-26 00:52:06 +0300

    New Animation Update
    +Added second loading animation.
    +Added hover and mouseover effects to buttons.

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
-const $color = document.querySelector('#colorChanger');
-const $root = document.querySelector('html');
+window.addEventListener('load', function () {
+    const $color = document.querySelector('#colorChanger');
+    const $root = document.querySelector('html');
 
-$color.addEventListener('click', function () {
+    $color.addEventListener('click', function () {
     let color = '#' + Math.round(Math.random() * 16777216).toString(16);
     $root.style.setProperty('--background-color', color);
-}, false);
+    }, false);
+
+    $animationButton = document.querySelector('#animationChanger');
+    $container = document.querySelector('.container');
+    $circles = document.querySelectorAll('.circle');
+
+    $animationButton.addEventListener('click', function () {
+        let state = $container.classList.toggle('container-rotate');
+        for(let one of $circles) {
+            one.classList.toggle('circle-rotate');
+        }
+        $root.style.setProperty('--pseudo-width', `${state ? 0 : '100%'}`);
+    });
+})
